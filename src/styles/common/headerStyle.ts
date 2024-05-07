63dcb46dcb3e36c4065bb361290d97fc98e634cc
김아영 <kimaydev@gmail.com>
2024-05-07 09:56:15 +0900

    헤더 스타일 수정

--- src/styles/common/headerStyle.ts
+++ src/styles/common/headerStyle.ts
@@ -1,16 +1,27 @@
 import styled from "styled-components";
 
 export const HeaderStyled = styled.header`
+  position: relative;
+  height: 60px;
   background: #000;
   nav {
     .list {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
       display: flex;
       align-items: center;
       justify-content: center;
       gap: 4px;
+      height: 100%;
+      & > li {
+        height: 100%;
+      }
       a {
-        display: block;
-        padding: 15px 20px;
+        display: flex;
+        align-items: center;
+        height: 100%;
+        padding: 0 20px;
         font-size: 20px;
         color: #fff;
       }
--- src/styles/layout/dashboardLayoutStyle.ts
+++ src/styles/layout/dashboardLayoutStyle.ts
@@ -4,7 +4,7 @@ export const DashboardLayoutStyled = styled.div`
   width: 100%;
   height: 100%;
   .contents-layout {
-    height: calc(100% - 50px);
+    height: calc(100% - 60px);
     padding: 10px;
     overflow: hidden;
   }
